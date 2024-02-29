c7f1603ff8b98470ce0804a0b2a73a6b30cc0b35
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-02-28 21:19:27 -0400

    [feat] setup tailwind styles.

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Makistour</h1>
+     <h1 class="text-4xl  font-bold">This is a heading using Playfair Display.</h1>
+<p class="text-base font-sans">This is a paragraph using Open Sans.</p>
+
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+      colors: {
+        bostonBlue: "#408abe", //Posible main color
+        fireBush: "#e98b27", //Posible main color
+        panache: "#f3faf6", //Posible main color
+        poloBlue: "#91b9cd",
+        chalky: "#eecf97",
+        osloGray: "#7c8d93",
+        spindle: "#c1e3ee",
+        submarine: "#b6c0c4",
+        limedSpruce: "#3c525a",
+      },
     },
   },
   plugins: [],
