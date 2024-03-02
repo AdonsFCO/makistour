e5dcd82438ef765823ccca8a1469c68d832f4722
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-03-02 10:24:07 -0400

    [feat] Add responsivenes to the header and include the burger Button

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,34 +1,49 @@
-import Image from "next/image"
-import Link from "next/link"
-import Hamburger from "./hamburger"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import Hamburger from "./hamburger";
+import react, { useState } from "react";
+import styles from './navStyles.module.css'
 
 export default function Header() {
-    return (<header>
-                 <Hamburger />
-            <div className="border-y-4 border-navyBlue">
-                <div className="bg-navyBlue">
-                <h2>
-                    Call us:
-                </h2>
-                <h2>+1 (849) 357-7580</h2>
-                <button>
-                    Buscar
-                </button>
-                    <input type="text"/>
-                </div>
+  const [navIsOpen, setNavOpen] = useState(false);
 
+  return (
+    <>
+      <header>
+        <div className="MOBILE-MENU flex lg:hidden">
+          <Image src="/logo.png" width={100} height={100} />
+          <Hamburger onClick={() => setNavOpen((prev) => !prev)} />
+
+          <nav className={navIsOpen ? styles.showMenuNav : styles.hideMenuNav }>
+            <Link href="./ofertas">Ofertas</Link>
+            <Link href="./destinos">destinos</Link>
+            <Link href="./blog">blog</Link>
+            <div onClick={ () => setNavOpen(!navIsOpen)}>X</div>
+          </nav>
+        </div>
+
+        <div className="DESKTOP-HEADER hidden lg:flex">
+          <div className="border-y-4 border-navyBlue">
+            <div className="bg-navyBlue">
+              <h2>Call us:</h2>
+              <h2>+1 (849) 357-7580</h2>
+              <button>Buscar</button>
+              <input type="text" />
             </div>
-            <div className=" bg-lightNavyBlue">
-            <Image src="/logo.png" width={100} height={100}/>
+          </div>
+          <div className=" bg-lightNavyBlue">
+            <Link href="./">
+              <Image src="/logo.png" width={100} height={100} />
+            </Link>
             <nav>
-                <Link href="./ofertas">Ofertas</Link>
-                <Link href="./destinos">destinos</Link>
-                <Link href="./blog">blog</Link>
-
+              <Link href="./ofertas">Ofertas</Link>
+              <Link href="./destinos">destinos</Link>
+              <Link href="./blog">blog</Link>
             </nav>
-            </div>
-    
-    </header>)
-
-
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
--- app/components/header/hamburger.jsx
+++ app/components/header/hamburger.jsx
@@ -1,9 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import react, { useState } from "react";
 
-const Hamburger = ({ isOpen, setIsOpen }) => {
+const Hamburger = ({ onClick }) => {
   return (
-    <FontAwesomeIcon icon={faBars} width="10px" />
+    <div>
+      <FontAwesomeIcon
+        icon={faBars}
+        width="10px"
+        className="animate-pulse"
+        onClick={onClick}
+      />
+    </div>
   );
 };
 
